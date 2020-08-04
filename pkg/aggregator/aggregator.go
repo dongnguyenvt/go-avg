bd882efb498bbce0fe598198496ad85bd3f902e6
Dong Nguyen <nguyenduydong@gmail.com>
2020-08-04 10:13:28 +0700

    division by zero

--- pkg/aggregator/aggregator.go
+++ pkg/aggregator/aggregator.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Aggregator interface {
-	Add(v float64)
+	Add(v float64) (float64, bool)
 	Sum() float64
 	Length() int
 	Avg() float64
@@ -19,8 +19,8 @@ type aggNoLimit struct {
 }
 
 type agg struct {
-	l     *list.List
-	max   int
+	l   *list.List
+	max int
 	aggNoLimit
 }
 
@@ -34,16 +34,19 @@ func NewAggregator(max int) Aggregator {
 	return &aggNoLimit{}
 }
 
-func (a *agg) Add(v float64) {
+func (a *agg) Add(v float64) (val float64, evicted bool) {
 	a.m.Lock()
 	a.sum += v
 	a.l.PushFront(v)
 	if a.l.Len() > a.max {
 		p := a.l.Back()
-		a.sum -= p.Value.(float64)
+		val = p.Value.(float64)
+		evicted = true
+		a.sum -= val
 		a.l.Remove(p)
 	}
 	a.m.Unlock()
+	return
 }
 
 func (a *agg) Length() int {
@@ -55,16 +58,20 @@ func (a *agg) Length() int {
 
 func (a *agg) Avg() float64 {
 	a.m.Lock()
-	ret := a.sum / float64(a.l.Len())
+	ret := 0.0
+	if a.l.Len() > 0 {
+		ret = a.sum / float64(a.l.Len())
+	}
 	a.m.Unlock()
 	return ret
 }
 
-func (a *aggNoLimit) Add(v float64) {
+func (a *aggNoLimit) Add(v float64) (float64, bool) {
 	a.m.Lock()
 	a.sum += v
 	a.count++
 	a.m.Unlock()
+	return 0, false
 }
 
 func (a *aggNoLimit) Sum() float64 {
@@ -83,7 +90,10 @@ func (a *aggNoLimit) Length() int {
 
 func (a *aggNoLimit) Avg() float64 {
 	a.m.Lock()
-	ret := a.sum / float64(a.count)
+	ret := 0.0
+	if a.count > 0 {
+		ret = a.sum / float64(a.count)
+	}
 	a.m.Unlock()
 	return ret
 }
